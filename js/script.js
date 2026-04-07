6287ddee4996cdc36368942236c970e141fcb0e7
ilnaz135 <ilnazmuftiev125@gmail.com>
2026-04-07 18:23:59 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,25 +4,21 @@ const scienceCard = document.querySelector('.publications-list')
 const tags = document.querySelector('.tags-cloud')
 const skillsCard = document.querySelector('.right-col').querySelector('.tags-cloud')
 
-const trimUserName = (username) => {
-    return username
-    .split(' ')
-    .map(nickname => nickname[0])
-    .join('')
+if (!localStorage.getItem('isLoggedIn')) {
+    window.location.href = 'loginindex.html'
 }
 
 async function getUserData(currentAccountId) {
     const response = await fetch("http://localhost:8000/api/v1/users")
     if(!response.ok) throw new Error('Ответ от сервера не получен.')
     const userData = {...await response.json()}
-    console.log(userData);
     const profileHTML = `
             <div class="profile-card">
                 <button class="edit-profile-btn" id="openModal">
                     <i class="fas fa-pencil-alt"></i>
                 </button>
 
-                <div class="avatar" id="profileAvatar">${trimUserName(userData[currentAccountId].username)}</div>
+                <div class="avatar" id="profileAvatar">${userData[currentAccountId].first_name[0] + userData[currentAccountId].last_name[0]}</div>
                 <div class="full-name">${userData[currentAccountId].first_name} ${userData[currentAccountId].last_name}</div>
                 <div class="username"><i class="fas fa-at"></i>${userData[currentAccountId].username}</div>
                 
